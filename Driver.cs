f6417ba22e0243a192bd9facfb52d0882781c4aa
CuongNguyenT85 <CuongNguyenT85@gmail.com>
2015-04-25 15:50:35 -0700

    Create Driver.cs

---
+++ Driver.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+
+namespace ExperianDemo
+{
+    public class Driver
+    {
+        public static IWebDriver Instance { get; set; }
+
+        public static void Initialize()
+        {
+            Instance = new FirefoxDriver();
+            Instance.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(3));
+        }
+        public static void Close()
+        {
+            Instance.Close();
+        }
+
+    }
+}
